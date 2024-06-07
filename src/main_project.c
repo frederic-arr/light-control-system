bd391c77ccf5271ef39064b70c9fb30e713f46ad
Frédéric Arroyo <frederic.arroyo@etu.hesge.ch>
2024-06-07 14:28:08 +0200

    lights

--- src/main_project.c
+++ src/main_project.c
@@ -16,6 +16,12 @@
 #include "dev/sprite.h"
 
 #include "sprites/bg_day.h"
+#include "sprites/bg_night.h"
+#include "sprites/light_veh_off.h"
+#include "sprites/light_veh_wait.h"
+#include "sprites/light_veh_ready.h"
+#include "sprites/light_veh_go.h"
+#include "sprites/light_veh_stop.h"
 
 void INT_INIT(void) {
 	ISER0 = (1 << 21) | (1 << 17); // EINT3, RTC
@@ -34,6 +40,15 @@ typedef struct {
 			g,
 			b;
 } color_t;
+
+uint8_t sequence[6][5] = {
+	{2,0, 2,2, 0},
+	{3,0, 3,2, 1},
+	{0,0, 0,2, 2},
+	{1,1, 0,3, 3},
+	{2,2, 0,0, 0},
+	{2,3, 1,1, 0},
+};
 
 int main(void)
 {
@@ -58,12 +73,30 @@ int main(void)
 	// RGB_LED_SET((color_t[]){{50, 51, 52}, {53, 54, 55}});
 	
 	bool was_dark = LUMINOSITY_IS_DARK();
+	uint16_t time = 0;
+	uint16_t state = 0;
+	uint8_t* light_veh[4] = {light_veh_wait, light_veh_ready, light_veh_go, light_veh_stop};
 	while (true)
 	{
 		bool is_dark = LUMINOSITY_IS_DARK(); 
 		if (is_dark != was_dark) {
-			DRAW_SPRITE(0, 0, 240, 320, 0x00, bg_day, is_dark ? 0.25 : 1.0);
+			DRAW_SPRITE(0, 0, 240, 320, 0x00, bg_day, is_dark ? 0.75 : 1.0);
 			was_dark = is_dark;
+		}
+
+		// CHA->GVA
+		DRAW_SPRITE(38, 70, 14, 32, 0x00, light_veh[sequence[state][0]], is_dark ? 0.75 : 1.0);
+		DRAW_SPRITE(68, 70, 14, 32, 0x00, light_veh[sequence[state][1]], is_dark ? 0.75 : 1.0);
+
+		// GVA->*
+		DRAW_SPRITE(90, 216, 14, 32, 0x00, light_veh[sequence[state][2]], is_dark ? 0.75 : 1.0);
+		DRAW_SPRITE(136, 216, 14, 32, 0x00, light_veh[sequence[state][3]], is_dark ? 0.75 : 1.0);
+
+		// VEG->GVA
+		DRAW_SPRITE(195, 135, 14, 32, 0x00, light_veh[sequence[state][4]], is_dark ? 0.75 : 1.0);
+		time = (time + 1) % 100;
+		if (time == 0) {
+			state = (state + 1) % 6;
 		}
 	}
 
